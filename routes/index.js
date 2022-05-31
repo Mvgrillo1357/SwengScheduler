798a37d206b4670c7bc2114e2e40d2af0d023fd0
Michael Grillo <58634459+Mvgrillo1357@users.noreply.github.com>
2022-05-30 22:35:08 -0400

    Adding a Dashboard/ Clean App
    adding a dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
-
 const express = require('express');
 const router  = express.Router();
+const {ensureAuthenticated} = require('../config/auth') 
 //login page
 router.get('/', (req,res)=>{
-    res.render('welcome');
+    res.render('welcome to SwengScheduler');
 })
 //register page
 router.get('/register', (req,res)=>{
     res.render('register');
 })
-
+router.get('/dashboard',ensureAuthenticated,(req,res)=>{
+    res.render('dashboard',{
+        user: req.user
+    });
+})
 module.exports = router; 
