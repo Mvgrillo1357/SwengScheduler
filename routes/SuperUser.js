188574515bdf05881fc56189f95e7ee546a60708
Michael Grillo <58634459+Mvgrillo1357@users.noreply.github.com>
2022-06-07 22:30:30 -0400

    debugging the superuser stuff

--- routes/SuperUser.js
+++ routes/SuperUser.js
@@ -1,65 +1,60 @@
 const express = require('express');
 const router = express.Router();
-const SuperUser = require("../models/SuperUser"); // assigning the exact DB
+const SuperUser = require("../models/SuperUser");
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 //login handle
-// router.get('/login',(req,res)=>{
-//     res.render('login');
-// })
-router.get('/create',(req,res)=>{
-    res.render('SuperUser')
+router.get('/login',(req,res)=>{
+    res.render('login');
+})
+router.get('/register',(req,res)=>{
+    res.render('register')
     })
 //Register handle
 router.post('/login',(req,res,next)=>{
 passport.authenticate('local',{
     successRedirect : '/dashboard',
-    failureRedirect: '/users/login', // need to be changed to the HR dashboard
+    failureRedirect: '/users/login',
     failureFlash : true
 })(req,res,next)
 })
   //register post handle
-  router.post('/create',(req,res)=>{
-    const { firstName, lastName, login, personalEmail, permissionLevel, proposedCompany, } = req.body;
+  router.post('/register',(req,res)=>{
+    const { firstName, lastName, login, personalEmail, permissionLevel, proposedCompany} = req.body;
     let errors = [];
-
-    console.log(' First Name: ' + firstName + ' Last Name :' + lastName + ' login :' + login  + ' Personal Email :' + personalEmail + ' Permission Level :' + 'Proposed Company :' + proposedCompany);
-    if(!firstName || !lastName || !login ||  !personalEmail || !proposedCompany) {
+    console.log(' First Name: ' + firstName + ' Last Name :' + lastName + ' login :' + login  + 
+    ' Personal Email :' + personalEmail + ' Permission Level :' + permissionLevel + 'Proposed Company :' + proposedCompany);
+    if(!firstName || !lastName || !login || !personalEmail || !permissionLevel || !proposedCompany) {
         errors.push({msg : "Please fill in all fields"})
     }
-
-
+    
+    //check if password is more than 6 characters
+    if(password.length < 6 ) {
+        errors.push({msg : 'password atleast 6 characters'})
+    }
     if(errors.length > 0 ) {
-    res.render('SuperUser', {
-        errors : errors,
+    res.render('register', {
         firstName : firstName, 
         lastName : lastName, 
         login : login,
         personalEmail: personalEmail,
         permissionLevel: permissionLevel,
-        proposedCompany: proposedCompany,
-        })
+        proposedCompany: proposedCompany})
      } else {
         //validation passed
-       SuperUser.findOne({$or : [ {personalEmail : personalEmail} ,{ login : login}]}).exec((err,superuser)=>{ // how to do or?
-        console.log(superuser);   
-        if(superuser) {
-            if( superuser.personalEmail == personalEmail){
-                errors.push({msg: 'email already registered'});
-            }
-            if( superuser.login == login) {
-                errors.push({msg: 'login already used'});
-            }
-            res.render('SuperUser',{errors, firstName, lastName, login, dateOfHire, permissionLevel, numberOfReports, personalEmail })  
-           } 
-           else {
+       User.findOne({email : email}).exec((err,user)=>{
+        console.log(user);   
+        if(user) {
+            errors.push({msg: 'email already registered'});
+            res.render('register',{errors,firstName,lastName,personalEmail,permissionLevel,proposedCompany, password})  
+           } else {
             const newSuperUser = new SuperUser({
                 firstName : firstName, 
                 lastName : lastName, 
                 login : login,
                 personalEmail: personalEmail,
                 permissionLevel: permissionLevel,
-                proposedCompany: proposedCompany,
+                proposedCompany: proposedCompany
             });
     
             //hash password
@@ -68,7 +63,7 @@ passport.authenticate('local',{
                 (err,hash)=> {
                     if(err) throw err;
                         //save pass to hash
-                        newSuperUser.password = hash;
+                        newUser.password = hash;
                     //save user
                     newSuperUser.save()
                     .then((value)=>{
