dd30d4ca44c4e66fb4fe2cd1868d6804838ed001
AvinaashGhansam <102967902+AvinaashGhansam@users.noreply.github.com>
2022-08-06 16:48:51 -0400

    Approve/Deny Organization Updates

--- models/Organization.js
+++ models/Organization.js
@@ -1,38 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const OrganizationSchema  = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Must Enter an organization's name"],
-    },
-    status: {
-        type: String,
-        enum: ['Pending', 'Approved', 'Denied'],
-        default: 'Pending',
-    },
-    // Who requested it
-    requestedBy: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'User',
-        required: true,
-    },
-    // Who approved it
-    approvedBy: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'User', 
-    },
+const OrganizationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Must Enter an organization's name"],
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Approved", "Denied"],
+    default: "Pending",
+  },
+  // Who requested it
+  requestedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  // Who approved it
+  approvedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
-OrganizationSchema.methods.approve = function(user) {
-    this.status = 'Approve';
-    this.approvedBy = user._id;
-    // @TODO Send a notification to the requestor letting them know their organization was approved.
+OrganizationSchema.methods.approve = function (user) {
+  this.status = "Approved";
+  this.approvedBy = user._id;
+  // @TODO Send a notification to the requestor letting them know their organization was approved.
+
+  const mailer = require("../config/mailer");
+  const mail = new mailer();
+  mail.sendMail({
+    to: this.requestedBy.personalEmail,
+    from: "no-reply@swengScheduler.com",
+    subject: "New Organization has been approved",
+    msg: `
+Hello ${this.requestedBy.firstName},
+a new organization has been approve with name: ${this.name} in SwengScheduler.`,
+  });
 };
 
-OrganizationSchema.methods.deny = function(user) {
-    this.status = "Denied";
-}
+OrganizationSchema.methods.deny = function (user) {
+  this.status = "Denied";
+  const mailer = require("../config/mailer");
+  const mail = new mailer();
+  mail.sendMail({
+    to: this.requestedBy.personalEmail,
+    from: "no-reply@swengScheduler.com",
+    subject: "New Organization has been denied",
+    msg: `
+Hello ${this.requestedBy.firstName},
+a new organization has been denied with name: ${this.name} in SwengScheduler.`,
+  });
+};
 
-const Organization = mongoose.model('Organization', OrganizationSchema);
+const Organization = mongoose.model("Organization", OrganizationSchema);
 
 module.exports = Organization;
--- routes/organization.js
+++ routes/organization.js
@@ -1,27 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const Organization = require("../models/Organization");
 
-router.get('/approve/:id', async (req,res) =>{
-    try {
-    const org = await Organization.findOne({_id: req.params.id});
-    org.status = "Approved";
+router.get("/approve/:id", async (req, res) => {
+  try {
+    const org = await Organization.findOne({ _id: req.params.id }).populate(
+      "requestedBy"
+    );
+
+    org.approve(req.user);
     org.save();
-    res.redirect('/dashboard');
-    }catch (e) {
-        res.redirect('/dashboard');
-    }
+    res.redirect("/dashboard");
+  } catch (e) {
+    console.log(e);
+    res.redirect("/dashboard");
+  }
 });
-router.get('/deny/:id', async (req,res) =>{
-    const org = await Organization.findOne({_id: req.params.id});
-    org.status = "Denied";
-    org.save();
-    res.redirect('/dashboard');
+router.get("/deny/:id", async (req, res) => {
+  const org = await Organization.findOne({ _id: req.params.id }).populate(
+    "requestedBy"
+  );
+  org.deny(req.user);
+  org.save();
+  res.redirect("/dashboard");
 });
-router.get('/delete/:id', async (req,res) =>{
-    const org = await Organization.deleteOne({_id: req.params.id});
-    req.flash('msg', 'organization was deleted');
-    res.redirect('/dashboard');
+router.get("/delete/:id", async (req, res) => {
+  const org = await Organization.deleteOne({ _id: req.params.id });
+  req.flash("msg", "organization was deleted");
+  res.redirect("/dashboard");
 });
 
-module.exports  = router;
+module.exports = router;
