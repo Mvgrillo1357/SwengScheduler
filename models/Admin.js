144869ffe7106514f6964322c2188ac1fd3341ad
AvinaashGhansam <102967902+AvinaashGhansam@users.noreply.github.com>
2022-08-07 12:22:27 -0400

    refined codes

--- models/Admin.js
+++ models/Admin.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
-const User = require('./user');
+const mongoose = require("mongoose");
+const User = require("./user");
 
-const Admin = User.discriminator('Admin', 
-    new mongoose.Schema(
-        // Custom SuperUser Fields here
-        { }, 
-        // What key to use for the discriminator
-        {discriminatorKey: 'role'}
-    )
+const Admin = User.discriminator(
+  "Admin",
+  new mongoose.Schema(
+    // Custom SuperUser Fields here
+    {},
+    // What key to use for the discriminator
+    { discriminatorKey: "role" }
+  )
 );
-    
+
 module.exports = Admin;
